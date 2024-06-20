b908f25c5145eabafe0e5fcd6c06476d0c803cb8
sebaboari <luciosebastianboari@gmail.com>
2024-06-20 16:28:55 -0300

    feat: responsive Welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -2,28 +2,30 @@ import { NavLink } from 'react-router-dom'
 
 const Welcome = () => {
   return (
-    <div className="flex items-center justify-center gap-8 mt-8">
-      <div className="flex flex-col gap-4">
-        <h1 className="font-serif text-3xl not-italic text-vanillaGray drop-shadow-lg">
+    <div className="flex items-center justify-center gap-8 mt-8 spxs:flex-col md:flex-row">
+      <div className="flex flex-col gap-4 splg:max-w-[320px] spxs:max-w-[280px] xl:max-w-[540px]">
+        <h1 className="font-serif not-italic lg:text-3xl text-vanillaGray drop-shadow-lg spxs:text-2xl">
           Las Meninas
         </h1>
-        <p className="font-serif font-normal not-italic text-5xl text-vanillaGray max-w-[540px] w-full h-auto drop-shadow-lg">
+        <p className="font-serif font-normal not-italic xl:text-5xl lg:text-3xl text-vanillaGray sm:max-w-[520px] w-full h-auto drop-shadow-lg spxs:text-2xl">
           <strong>Casa de Antigüedades</strong>, muebles, iluminación, arte y{' '}
           <strong>eso especial que buscas</strong> para tu hogar.
         </p>
 
-        <div className="flex items-center self-end gap-6">
-          <p className="font-sans text-xs not-italic font-normal text-vanillaGray">
-            ¿Tienes antigüedades que quieras vender?{' '}
+        <div className="flex w-full gap-6 xl:items-center xl:self-end spxs:flex-col spxs:self-start spxs:items-start xl:flex-row">
+          <div className="w-full xl:w-fit spxs:flex spxs:flex-col xl:flex-row xl:gap-1">
+            <p className="font-sans text-xs not-italic font-normal text-vanillaGray">
+              ¿Tienes antigüedades que quieras vender?{' '}
+            </p>
             <NavLink
-              className="transition duration-300 text-vanillaGray/60 hover:text-blackened"
+              className="font-sans text-xs not-italic font-normal transition duration-300 text-vanillaGray/60 hover:text-blackened"
               to="/contacto"
             >
               Mándanos un mensaje
             </NavLink>
-          </p>
+          </div>
           <NavLink
-            className="px-6 py-3 font-serif text-base not-italic font-normal transition duration-300 rounded-full text-vanillaGray bg-militar drop-shadow hover:shadow-lg hover:bg-blackened hover:text-vanilla"
+            className="px-6 py-3 font-serif text-base not-italic font-normal transition duration-300 rounded-full md:self-end text-vanillaGray bg-militar drop-shadow hover:shadow-lg hover:bg-blackened hover:text-vanilla"
             to="/productos"
           >
             Ver catálogo
@@ -31,7 +33,7 @@ const Welcome = () => {
         </div>
       </div>
 
-      <figure className="max-w-[440px] grid place-items-center">
+      <figure className="md:max-w-[320px] xl:max-w-[440px] grid place-items-center spxs:max-w-[280px] lg:max-w-[380px]">
         <img
           className="object-cover"
           src="https://firebasestorage.googleapis.com/v0/b/las-meninas-d7387.appspot.com/o/fornitures-compress.png?alt=media&token=0bbe1c64-059f-473f-bdb1-1b33fa4ed95d"
