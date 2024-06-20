67f4f5697c0c2431724d4aa3a0d1adac80061337
sebaboari <luciosebastianboari@gmail.com>
2024-06-19 22:20:53 -0300

    feat: extends screens breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,11 @@ export default {
         militar: '#403D2E',
         blackened: '#26211E',
       },
+      screens: {
+        spxs: '320px',
+        spsm: '375px',
+        splg: '480px',
+      },
     },
   },
   plugins: [],
