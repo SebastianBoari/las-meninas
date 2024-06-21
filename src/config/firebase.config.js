f5e6e2b96e5aa39e4f26b09e233a3a5a5ee18817
sebaboari <luciosebastianboari@gmail.com>
2024-06-21 09:36:59 -0300

    feat: add firebase/auth

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 import { firebaseVariables } from './env.config'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: firebaseVariables.apiKey,
@@ -15,5 +16,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 const storage = getStorage(app)
+const auth = getAuth(app)
 
-export { app, db, storage }
+export { app, db, storage, auth }
