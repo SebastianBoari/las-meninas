496f61c8d904467831b425ee3b6f621ceeb8ba17
sebaboari <luciosebastianboari@gmail.com>
2024-06-23 01:18:20 -0300

    feat: contact map

--- src/components/ContactMap.jsx
+++ src/components/ContactMap.jsx
@@ -5,12 +5,12 @@ const ContactMap = () => {
   const position = { lat: -33.011059, lng: -58.518454 }
 
   return (
-    <div className="h-[280px] md:w-[380px]  spxs:w-[240px] spsm:w-[280px] splg:w-[320px]">
-      {/* <APIProvider apiKey={firebaseVariables.apiKey}>
+    <div className="h-[280px] md:w-[380px] spxs:w-[240px] spsm:w-[280px] splg:w-[320px]">
+      <APIProvider apiKey={firebaseVariables.apiKey}>
         <Map defaultCenter={position} defaultZoom={16}>
           <Marker position={position} />
         </Map>
-      </APIProvider> */}
+      </APIProvider>
     </div>
   )
 }
