430d7b2ef2d3f21429213f6c87a26094251bc1f6
sebaboari <luciosebastianboari@gmail.com>
2024-07-05 19:56:38 -0300

    fix: quit async

--- src/components/Home/Welcome.jsx
+++ src/components/Home/Welcome.jsx
@@ -1,14 +1,15 @@
 import { NavLink } from 'react-router-dom'
 
-const Welcome = async () => {
+const Welcome = () => {
   return (
     <div className="flex items-center justify-center gap-8 mt-8 spxs:flex-col md:flex-row">
       <div className="flex flex-col gap-4 splg:max-w-[320px] spxs:max-w-[280px] xl:max-w-[540px]">
         <h1 className="not-italic font-primary lg:text-3xl text-vanillaGray drop-shadow-lg spxs:text-2xl">
           Las Meninas
         </h1>
+
         <p className="font-primary font-normal not-italic xl:text-5xl lg:text-3xl text-vanillaGray sm:max-w-[440px] w-full h-auto drop-shadow-lg spxs:text-2xl">
-          <strong>Casa de Antigüedades</strong>, muebles, iluminación, arte y{' '}
+          <strong>Casa de Antigüedades</strong>, muebles, iluminación, arte y
           <strong>eso especial que buscas</strong> para tu hogar.
         </p>
 
@@ -17,6 +18,7 @@ const Welcome = async () => {
             <p className="text-xs not-italic font-normal font-secondary text-vanillaGray">
               ¿Tienes antigüedades que quieras vender?{' '}
             </p>
+
             <NavLink
               className="text-xs not-italic font-normal transition duration-300 font-secondary text-vanillaGray/60 hover:text-blackened"
               to="/contacto"
@@ -24,6 +26,7 @@ const Welcome = async () => {
               Mándanos un mensaje
             </NavLink>
           </div>
+
           <NavLink
             className="px-6 py-3 text-base not-italic font-normal transition duration-300 rounded-full font-primary md:self-end text-vanillaGray bg-militar drop-shadow hover:shadow-lg hover:bg-blackened hover:text-vanilla"
             to="/productos"
