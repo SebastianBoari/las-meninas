5dac47879ad7e652b305132f00d8e70774c4d31f
sebaboari <luciosebastianboari@gmail.com>
2024-07-06 21:42:32 -0300

    feat: add modal portal and warning dialog for deleting a product

--- src/components/Admin/Products/ProductTableRow.jsx
+++ src/components/Admin/Products/ProductTableRow.jsx
@@ -1,3 +1,8 @@
+import { useState } from 'react'
+import useFirestoreDeleteDocument from '@hooks/useFirestoreDeleteDocument'
+import Modal from '@components/Common/Modal'
+import WarnIcon from '@components/Partials/Icons/WarnIcon'
+
 const ProductTableRow = ({
   id,
   title,
@@ -6,6 +11,21 @@ const ProductTableRow = ({
   currency,
   price,
 }) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const { deleteDoc, loading, error, success } = useFirestoreDeleteDocument()
+
+  const handleDeleteProduct = () => {
+    deleteDoc('products', id)
+
+    if (error) {
+      alert('Ha ocurrido un error al intentar eliminar el producto ', error)
+    }
+
+    if (success) {
+      setIsOpen(false)
+    }
+  }
+
   return (
     <tr
       key={id}
@@ -30,11 +50,44 @@ const ProductTableRow = ({
           Editar
         </a>
         <button
+          onClick={() => setIsOpen(true)}
           href="#"
           className="p-1 font-medium text-white transition duration-300 bg-red-600 rounded-md hover:bg-red-500"
         >
           Eliminar
         </button>
+
+        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+          <div className="flex flex-col items-center gap-2">
+            <figure className="text-amber-500">
+              <WarnIcon width={'64px'} height={'64px'} />
+            </figure>
+            <div className="flex flex-col items-center gap-2">
+              <h4 className="text-lg font-medium text-center text-blackened">
+                Cuidado
+              </h4>
+              <p className="text-center max-w-80 text-blackened">
+                Seguro que quiere eliminar el producto <strong>{title}</strong>?
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-end w-full gap-6">
+            <button
+              className="px-4 py-2 font-medium transition duration-300 rounded text-blackened text-md hover:underline"
+              onClick={() => setIsOpen(false)}
+            >
+              Cancelar
+            </button>
+
+            <button
+              className="px-4 py-2 font-medium text-white transition duration-300 bg-red-800 rounded text-md hover:bg-red-600"
+              onClick={handleDeleteProduct}
+            >
+              {loading ? 'Eliminando...' : 'Eliminar'}
+            </button>
+          </div>
+        </Modal>
       </td>
     </tr>
   )
--- src/components/Common/Modal.jsx
+++ src/components/Common/Modal.jsx
@@ -1,3 +1,28 @@
-const Modal = () => {}
+import ReactDom from 'react-dom'
+import CrossIcon from '@components/Partials/Icons/CrossIcon'
+
+const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null
+
+  return ReactDom.createPortal(
+    <div className="fixed inset-0 z-50 bg-black bg-opacity-70">
+      <div className="fixed z-50 p-2 transform -translate-x-1/2 -translate-y-1/2 bg-white rounded-md top-1/2 left-1/2 min-w-96">
+        <div className="flex flex-col items-center justify-between w-full h-full">
+          <button
+            onClick={() => onClose()}
+            className="self-end p-1 transition duration-300 rounded-full text-blackened hover:text-red-600"
+          >
+            <CrossIcon width={'24px'} height={'24px'} />
+          </button>
+
+          <div className="flex flex-col items-center justify-between w-full gap-6 p-2">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>,
+    document.getElementById('portal')
+  )
+}
 
 export default Modal
