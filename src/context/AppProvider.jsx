c1358dbc619431573d3502e2625d3d3e3fe36cf4
sebaboari <luciosebastianboari@gmail.com>
2024-07-23 12:46:44 -0300

    feat: AppProvider AppContext

---
+++ src/context/AppProvider.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect, createContext } from 'react'
+import useFirestoreGetDocuments from '@hooks/useFirestoreGetDocuments'
+
+const AppContext = createContext()
+
+const AppProvider = ({ children }) => {
+  const [products, setProducts] = useState(null)
+
+  const { data, loading, error } = useFirestoreGetDocuments('products')
+
+  useEffect(() => {
+    if (data) {
+      setProducts(data)
+    }
+  }, [data])
+
+  return (
+    <AppContext.Provider value={{ products, loading, error }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export { AppContext, AppProvider }
